dfb45e6898ab952e512e5fdab03d78279f6b09ce
Shuks <babatundeshukurat2001@gmail.com>
2026-09-17 21:41:20 +0100

    class drill

---
+++ java32/ClassTasks/Factorial.java
@@ -0,0 +1,22 @@
+//write a program that collect a number as an integer but return it as long:
+//input:5
+//output:120
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        
+        long result = 1;
+        
+        for (int count = 1; count <= number; count++) {
+            result = result * count;
+        }
+        
+        System.out.println(result);
+    }
+}
